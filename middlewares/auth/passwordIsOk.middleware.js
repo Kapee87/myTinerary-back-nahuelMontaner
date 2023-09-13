b552478a58d17cbee12b5b0252279b6cc5da2a3a
Kapee87 <kapserker@gmail.com>
2023-09-13 16:31:37 -0300

    minor fix to sign in

--- middlewares/auth/passwordIsOk.middleware.js
+++ middlewares/auth/passwordIsOk.middleware.js
@@ -6,8 +6,11 @@ const passwordIsOk = async (req, res, next) => {
         const db_pass = req.user.password
         const form_pass = req.body.password
         console.log(db_pass, form_pass, bcryptjs.hashSync(form_pass));
+        console.log(bcryptjs.compareSync(form_pass, db_pass));
         if (bcryptjs.compareSync(form_pass, db_pass)) return next()
-
+        return res.status(400).json({
+            message: 'invalid credentials'
+        })
     } catch (error) {
         return next(error)
     }
